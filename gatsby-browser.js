fc3eba94bfa9bfd4339d948182f93b8674ccae5a
dskvr <dskvr@users.noreply.github.com>
2019-11-29 16:38:59 +0100

    use react-simple-flowchart,implement onPageLoad

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,45 +1,17 @@
-exports.onInitialClientRender = (n, options) => {
-  // console.info(`options: ${options}`)
-  // see https://developer.mozilla.org/en-US/docs/Web/API/HTMLScriptElement
-  // https://www.html5rocks.com/en/tutorials/speed/script-loading/
-  var importScript = (function (oHead) {
-    function loadError (oError) {
-      throw new URIError("The script " + oError.target.src + " is not accessible.");
-    }
-    return function (sSrc, fOnload) {
-      var oScript = document.createElement("script");
-      oScript.type = "text\/javascript";
-      oScript.onerror = loadError;
-      if (fOnload) { oScript.onload = fOnload; }
-      oHead.appendChild(oScript);
-      oScript.src = sSrc;
-    }
-  })(document.head || document.getElementsByTagName("head")[0]);
+import React from 'react';
+import { render } from 'react-dom';
+import Flowchart from 'react-simple-flowchart';
 
-importScript("https://cdn.bootcss.com/raphael/2.2.7/raphael.min.js", function () {
-  importScript("http://flowchart.js.org/flowchart-latest.js", function () {
-    // importScript("https://cdn.bootcss.com/jquery/3.2.1/jquery.min.js", function () {
-      // $.fn.flowchart = function( options ) {
-      //   return this.each(function() {
-      //     var $this = $(this);
-      //     // should not use innerHTML because its content is escaped!
-      //     // document.getElementsByClassName("flowchart")[0].childNodes[0].nodeValue;
-      //     var diagram = flowchart.parse($this.text());
-      //     $this.html('');
-      //     diagram.drawSVG(this, options);
-      //   });
-      // };
-      // $(".flowchart").flowchart();
-      var flowchartElements = document.getElementsByClassName("flowchart");
-      var flowchartElementsCount = flowchartElements.length;
-      for (var i = 0; i < flowchartElementsCount; i++) {
-        // https://stackoverflow.com/questions/1912501/unescape-html-entities-in-javascript
-        // use childNodes[0].nodeValue instead of text() of jquery
-        var diagram = flowchart.parse(flowchartElements[i].childNodes[0].nodeValue);
-        flowchartElements[i].childNodes[0].nodeValue="";
-        diagram.drawSVG(flowchartElements[i], options);
-      }
-    // });
-  });
- });
+render()
+
+export function onRouteUpdate({ location, prevLocation } = {}, options){
+  function onError(error) {
+    console.log(error);
+  }
+  var els = document.getElementsByClassName("flowchart");
+  console.log(els)
+  var c = els.length;
+  for (var i = 0; i < c; i++) {
+    render(<Flowchart chartCode={els[i].childNodes[0].nodeValue} options={options} onError={onError} />, els[i])
+  }
 }
